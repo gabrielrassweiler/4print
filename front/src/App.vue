5aca4adb883adc01a760d4305a6d666c80da0ced
Gabriel <gabriel@conectra.com.br>
2022-05-18 17:18:45 -0300

    feat(nav,eslint): remove eslint, add navbar e router links

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p class="logo">4PRINT</p>
+  </div>
+  <div id="nav">
+    <router-link to="/" class="router">INÍCIO</router-link>
+    <router-link to="/sobre" class="router">SOBRE</router-link>
+    <router-link to="/contato" class="router">CONTATO</router-link>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.logo {
+  display: flex;
+  color: white;
+  width: 100px;
+  position: relative;
+  z-index: 1;
+  padding: 16px 0;
+  margin-left: 15%;
+  font-size: 20px;
+}
+
+.router {
+  margin-right: 20px;
+}
+
+#nav {
+  padding: 16px 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgb(30, 30, 30);
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  a {
+    color: white;
+    text-decoration: none;
+    transition: color .15s;
+    font-size: .77em;
+
+    &.router-link-exact-active {
+      color: #007bff;
+    }
+  }
+  a:hover{
+    color: #1988ff;
+  }
 }
 </style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="scss">
+
+</style>
